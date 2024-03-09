22f7c81d99fdbb38ae43920861482243447afbb9
GwinBest <thegwinbest@gmail.com>
2024-03-09 16:36:26 +0300

    refactoring

--- lab1/src/main/java/org/javalabs/lab1/Lab1Application.java
+++ lab1/src/main/java/org/javalabs/lab1/Lab1Application.java
@@ -2,8 +2,11 @@ package org.javalabs.lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableWebMvc
 public class Lab1Application {
     public static void main(String[] args) {
         SpringApplication.run(Lab1Application.class, args);
