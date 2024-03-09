a895294028d2c09c5320143abd22e4f5f00c4fc2
GwinBest <thegwinbest@gmail.com>
2024-03-09 21:51:35 +0300

    refactoring

--- lab1/src/main/java/org/javalabs/lab1/SwaggerConfig.java
+++ lab1/src/main/java/org/javalabs/lab1/SwaggerConfig.java
@@ -11,7 +11,6 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-
 @Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
