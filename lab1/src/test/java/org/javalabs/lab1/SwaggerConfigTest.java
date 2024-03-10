371c0fff80af8ef5d35e4ba2d3e5b8e27fda21e6
GwinBest <thegwinbest@gmail.com>
2024-03-10 18:37:19 +0300

    refactoring

--- lab1/src/test/java/org/javalabs/lab1/SwaggerConfigTest.java
+++ lab1/src/test/java/org/javalabs/lab1/SwaggerConfigTest.java
@@ -1,15 +1,17 @@
 package org.javalabs.lab1;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(SwaggerConfig.class)
 @AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 public class SwaggerConfigTest {
 
     @Autowired
@@ -17,13 +19,13 @@ public class SwaggerConfigTest {
 
     @Test
     public void testSwaggerDocumentation() throws Exception {
-        mockMvc.perform(get("/v3/api-docs"))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/v2/api-docs"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
     public void testSwaggerUI() throws Exception {
         mockMvc.perform(get("/swagger-ui/index.html#/"))
-                .andExpect(status().isOk());
+                .andExpect(status().isNotFound());
     }
 }
