2ea7d3fbcffbabd7e9dd6f7be390456e0705586d
GwinBest <thegwinbest@gmail.com>
2024-03-12 16:09:12 +0300

    refactoring

--- lab1/src/main/java/org/javalabs/lab1/exceptionhandler/GlobalExceptionHandler.java
+++ lab1/src/main/java/org/javalabs/lab1/exceptionhandler/GlobalExceptionHandler.java
@@ -9,33 +9,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
-        LOGGER.error("500 Method Not Allowed - {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("500: Internal Server Error");
+        LOGGER.error("500 Internal Server Error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("500 Internal Server Error");
     }
 
     @ExceptionHandler({HttpClientErrorException.class})
     public ResponseEntity<Object> handleHttpClientErrorException(HttpClientErrorException ex, WebRequest request) {
-        LOGGER.error("400 Method Not Allowed - {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("400: Bad Request");
+        LOGGER.error("400 Bad Request");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("400 Bad Request");
     }
 
-    @ExceptionHandler({NoResourceFoundException.class})
-    public ResponseEntity<Object> handleNoResourceFoundException(NoResourceFoundException ex, WebRequest request) {
-        LOGGER.error("404 Method Not Allowed - {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("404: Not Found");
+    @ExceptionHandler({NoHandlerFoundException.class})
+    public ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, WebRequest request) {
+        LOGGER.error("404 Not Found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("404 Not Found");
     }
 
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
     public ResponseEntity<Object> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException ex,
                                                                     WebRequest request) {
-        LOGGER.error("405 Method Not Allowed - {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body("405: Method Not Allowed");
+        LOGGER.error("405 Method Not Allowed");
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body("405 Method Not Allowed");
     }
 }
--- lab1/src/test/java/org/javalabs/lab1/exceptionhandler/GlobalExceptionHandlerTest.java
+++ lab1/src/test/java/org/javalabs/lab1/exceptionhandler/GlobalExceptionHandlerTest.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -15,15 +15,15 @@ import static org.mockito.Mockito.mock;
 
     @Test
     void testHandleNoResourceFoundException() {
-        NoResourceFoundException ex = mock(NoResourceFoundException.class);
+        NoHandlerFoundException ex = mock(NoHandlerFoundException.class);
         WebRequest request = mock(WebRequest.class);
 
         GlobalExceptionHandler handler = new GlobalExceptionHandler();
 
-        ResponseEntity<Object> responseEntity = handler.handleNoResourceFoundException(ex, request);
+        ResponseEntity<Object> responseEntity = handler.handleNoHandlerFoundException(ex, request);
 
         assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
-        assertEquals("404: Not Found", responseEntity.getBody());
+        assertEquals("404 Not Found", responseEntity.getBody());
     }
 
     @Test
